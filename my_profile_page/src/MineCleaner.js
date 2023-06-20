711e3208e62e3584ad59a999ebe1a2dce35f27a0
Jason <jason5donoghue@gmail.com>
2023-06-20 12:50:37 +0100

    updated links for all projects

--- my_profile_page/src/MineCleaner.js
+++ my_profile_page/src/MineCleaner.js
@@ -1,7 +1,9 @@
 export default function MineCleaner() {
     return (
+        <a className="minecleanerlink" href="https://github.com/Jason5D/Mine_Cleaner" target="_blank" rel="noopener noreferrer">
         <button className="minecleaner">
             <h3>Mine Cleaner, under construction...</h3>
         </button>
+        </a>
     )
 }
--- my_profile_page/src/PersonalPantry.js
+++ my_profile_page/src/PersonalPantry.js
@@ -1,7 +1,9 @@
 export default function PersonalPantry() {
     return (
+        <a className="personalpantrylink" href="https://github.com/Jason5D/Personal_Pantry" target="_blank" rel="noopener noreferrer">
         <button className="personalpantry">
             <h3>Personal Pantry, under construction...</h3>
         </button>
+        </a>
     )
 }
