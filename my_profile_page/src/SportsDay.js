c0c458e96512995cdc168fb64be7abe7f32c1e7e
Jason <jason5donoghue@gmail.com>
2023-07-14 10:27:25 +0100

    added sports day deployed site

--- my_profile_page/src/SportsDay.js
+++ my_profile_page/src/SportsDay.js
@@ -1,9 +1,14 @@
 export default function SportsDay() {
-    return (
-        <a className="sportsdaylink" href="https://github.com/Jason5D/Sports_Day" target="_blank" rel="noopener noreferrer">
-        <button className="sportsday">
-            <h3>Sports day, under construction...</h3>
-        </button>
-        </a>
-    )
+  return (
+    <a
+      className="sportsdaylink"
+      href="https://sportsday.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button className="sportsday">
+        <h3>Sports day, under construction...</h3>
+      </button>
+    </a>
+  );
 }
